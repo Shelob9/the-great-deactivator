71f1a4b5b13d46ef64fcde7dec5ee1dce261dea9
Josh <JPollock412@gmail.com>
2013-09-13 18:44:30 -0400

    actual plugin file

---
+++ the-great-deactivator.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: The Great Deactivator
+Plugin URI: https://github.com/Shelob9/the-great-deactivator
+Description: A plugin designed for removing things from my Second Foundation theme.
+Version: 0.1
+Author: Josh Pollock
+Author URI: http://JoshPress.net
+License: GLPv3
+License URI: http://www.gnu.org/licenses/gpl.html
+*/
+include('inc/theGreatDeactivator.php');
